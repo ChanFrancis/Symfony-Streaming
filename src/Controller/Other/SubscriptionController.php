54d4212ee83a766f24315b3ec2adf7f47551355f
francis <francis@beetogreen.com>
2024-12-17 14:22:19 +0100

    update 17/12

--- src/Controller/Other/SubscriptionController.php
+++ src/Controller/Other/SubscriptionController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Other;
 
+use App\Repository\SubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,28 @@ use Symfony\Component\Routing\Attribute\Route;
 class SubscriptionController extends AbstractController
 {
     #[Route('/subscriptions', name: 'page_subscription')]
-    public function index(): Response
+    public function index(SubscriptionRepository $subscriptionRepository): Response
     {
-        return $this->render('other/abonnements.html.twig');
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createAccessDeniedException('You must be logged in to access this page.');
+        }
+
+        $subscriptionId = $user->getCurrentSubscription();
+
+        if (!$subscriptionId) {
+            throw $this->createNotFoundException('No subscription associated with this user.');
+        }
+
+        $subscription = $subscriptionRepository->find($subscriptionId);
+
+        if (!$subscription) {
+            throw $this->createNotFoundException('Subscription not found.');
+        }
+
+        return $this->render('other/abonnements.html.twig', [
+            'subscription' => $subscription,
+        ]);
     }
 }
